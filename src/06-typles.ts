7820e59e67697aaa6b9290c708a5b78da9d3a7db
Anar Agaev <anar.n.agaev@gmail.com>
2023-12-02 20:06:52 +0300

    Added tuple type

---
+++ src/06-typles.ts
@@ -0,0 +1,7 @@
+const pairs: [string, string][] = [ ['key', 'value'], ['key2', 'value2'] ];
+
+const data: [number, boolean, string] = [1, true, 'lodash'];
+
+// csv
+const doc: [string, string, number, Date][] = [];
+doc.push(['Thomas', 'Simon', 37, new Date(1985, 7, 8)]);
