03c6b8f54aa1acdf0fc1511c2e9ec2a0c7274b6d
Anar Agaev <anar.n.agaev@gmail.com>
2023-12-04 18:15:17 +0300

    Added alias type

---
+++ src/07-aliases.ts
@@ -0,0 +1,14 @@
+// Type or alias
+type MyBoolean = false | true
+
+type Pair = [string, string]
+type Pairs = Pair[]
+const pairMatrix: Pairs[] = []
+
+type Machine = {
+    brand: 'string'
+    type: 'string'
+    dors: number
+    free: boolean
+}
+const cars: Machine[] = []
