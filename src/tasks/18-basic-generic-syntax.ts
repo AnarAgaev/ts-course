1e4aeec2d3e0a2d6d9fd040c2fce6f7c2f1a8d17
Anar Agaev <anar.n.agaev@gmail.com>
2024-06-06 13:00:30 +0300

    Edded arrow function generic example

--- src/tasks/18-basic-generic-syntax.ts
+++ src/tasks/18-basic-generic-syntax.ts
@@ -1,6 +1,10 @@
-export function append<T> (el: T, list: T[]): T[] {
+export function append <T> (el: T, list: T[]): T[] {
     return list.concat(el)
 }
 
 append(1,  [2, 4, 6, 8])
 // append('some sting', [1, 2, 3, 4]) // Has error. All the vars should be single type
+
+
+// This func rewritten on Arrow function
+export const prepend = <T> (el: T, arr: T[]): T[] => arr.concat(el)
